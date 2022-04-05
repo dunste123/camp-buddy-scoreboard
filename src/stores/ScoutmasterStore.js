7a679f7fc50fe76832525fa7787769e31e57ae6a
duncte123 <contact@duncte123.me>
2022-04-05 13:34:28 +0200

    Top/bottom for goro and aiden apparently

--- src/stores/ScoutmasterStore.js
+++ src/stores/ScoutmasterStore.js
@@ -7,12 +7,32 @@ export default class ScoutmasterStore extends BaseStore {
                 name: "Aiden",
                 emoteId: "514293666936782850",
                 score: 0,
+                extra_scores: [
+                    {
+                        title: "Top",
+                        score: 0,
+                    },
+                    {
+                        title: "Bottom",
+                        score: 0,
+                    },
+                ],
             },
 
             {
                 name: "Goro",
                 emoteId: "514293666861416466",
                 score: 0,
+                extra_scores: [
+                    {
+                        title: "Top",
+                        score: 0,
+                    },
+                    {
+                        title: "Bottom",
+                        score: 0,
+                    },
+                ],
             },
         ], "scoutmasters");
     }
